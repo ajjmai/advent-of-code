f77c222dfa30f1ee2fb7cfb46bd415e32824bc65
Aija Valleala <aija.valleala@solita.fi>
2024-12-01 10:09:51 +0200

    update solution templates

--- src/y2024/Day00.kt
+++ src/y2024/Day00.kt
@@ -1,4 +1,4 @@
-package y2023
+package y2024
 
 import readInput
 
@@ -15,13 +15,13 @@ fun main() {
         return 0
     }
 
-    val testInput = readInput("Day0_test", "y2023")
+    val testInput = readInput("Day0_test", "y2024")
     println(part1(testInput))
 //    check(part1(testInput) == 142)
 //    println(part2(testInput))
 //    check(part2(testInput) == 309)
 
-    val input = readInput("Day0", "y2023")
+    val input = readInput("Day0", "y2024")
 //    println(part1(input))
 //    println(part2(input))
 }
